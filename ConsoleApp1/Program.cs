430422701b547dc5cd6a154ea6102630a776a587
vince <vince@MR-V-G>
2023-04-04 14:33:54 +0200

    Aggiungere i file di progetto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string[] Foods = {"Pane", "Acqua", "Ciccio", "Anguria", "Arancia", "Pasta", "Pizza"};
+
+Console.WriteLine($"N: {Foods.Length}");
+
+foreach (string Food in Foods)
+{
+    Console.WriteLine(Food);
+}
+
+
+Console.WriteLine($"Primo Elemento: {Foods[(Foods.Length -1) - (Foods.Length - 1)]}");
+Console.WriteLine($"Ultimo Elemento: {Foods[Foods.Length-1]}");
+
+Console.ReadLine();
